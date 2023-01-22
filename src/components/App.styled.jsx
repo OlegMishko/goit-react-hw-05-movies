695caf8b574c99a71bcd8780cccf50c96a87170e
OlegMishko <oleg.mishko@gmail.com>
2023-01-22 17:49:49 +0200

    base

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const MainTitle = styled.h1`
+  font-size: ${p => p.theme.fontSizes.l};
+  font-weight: ${p => p.theme.fontWeights.bold};
+  text-align: center;
+`;
+
+export const Title = styled.h2`
+  margin-top: 10px;
+  text-align: center;
+`;
